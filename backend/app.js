6589cc074e7fc6c3980b66cf8b3e76bc743b2599
nibaldo <nibaldo.perot.quevedo@gmail.com>
2018-07-03 18:17:16 -0400

    agrego CORS

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,13 @@ var user_routes = require('./routes/user');
 // Middlewares de body-parser ( se ejecuta antes de una petición http)
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, X-Rrequested-With,Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Allow', 'GET,POST,PUT,DELETE,OPTIONS');
+    next();
+})
 // Configurar cabeceras y cors
 
 // rutas
